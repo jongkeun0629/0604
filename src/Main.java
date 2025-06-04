476a0416888fda0bfe8523838a2f32c33e6f773c
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-04 14:51:09 +0900

    do while-switch

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        int input = 1;
 
-        for (int i = 1; i < 10; i++){
-            System.out.println(i + "단");
-            for (int j = 1; j < 10; j++){
-                System.out.println(i + " * " + j + " = " + (i * j));
+        do {
+            System.out.println("안녕하세요 자판기 입니다.");
+
+            switch (input) {
+                case 1:
+                    System.out.println("콜라");
+                    break;
+                case 2:
+                    System.out.println("사이다");
+                    break;
+                case 3:
+                    System.out.println("물");
+                    break;
+                default:
+                    System.out.println("잘못 고름ㅋ");
             }
-            System.out.println();
-        }
+
+            input = 0;
+        } while(input != 0);
     }
 }
