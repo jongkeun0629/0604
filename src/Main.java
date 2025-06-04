0431147c18ab4f766724d549c3d4fcf7a57dd35b
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-04 11:45:41 +0900

    for-if

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 7;
-
-        System.out.println("a == b : " + (a == b));
-        System.out.println("a != b : " + (a != b));
-
-        System.out.println("a > b : " + (a > b));
-        System.out.println("a < b : " + (a < b));
-        System.out.println("a >= 10 : " + (a >= 10));
-        System.out.println("b <= 5 : " + (b <= 5));
-
+        for (int i = 20; i > 0; i--) {
+            if(i % 4 == 0){
+                System.out.println("i : " + i);
+            }
+        }
     }
 }
