c80411c648ea5637d7c69a8a8d24fb0295b52832
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-04 14:07:23 +0900

    double for

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 20; i > 0; i--) {
-            if(i % 4 == 0){
-                System.out.println("i : " + i);
+
+        for (int i = 1; i < 10; i++){
+            System.out.println(i + "단");
+            for (int j = 1; j < 10; j++){
+                System.out.println(i + " * " + j + " = " + (i * j));
             }
+            System.out.println();
         }
     }
 }
